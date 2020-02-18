0cb2cc65eefdc0bf043f6a888ba8d9c3a619841d
erikasari <56011377+erikasari@users.noreply.github.com>
2020-02-18 08:32:05 +0700

    explicit Inten

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -1,15 +1,31 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText nameInput;
+    private TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handleExplicit(View view) {
+        Intent intent =  new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleExit(View view) {
+        finish();
+    }
+
 
 }
